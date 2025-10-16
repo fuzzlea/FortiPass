12594cc8fc3be774a59a2364ecac0b2dd7bc1208
aj <fuzzleaofficial@gmail.com>
2025-10-16 18:08:05 -0400

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,11 +2,11 @@ import { useReadLocalStorage } from "usehooks-ts"
 
 export default function DashboardPage() {
 
-    const currentUser = useReadLocalStorage('user');
+    const currentUser = useReadLocalStorage<{ user: '', password: '' }>('user');
 
     return (<>
 
-        <div className="w-full h-screen items-center px-8 pt-8">
+        <div className="w-full h-screen items-center px-8 pt-14">
 
             <p className="text-6xl font-bold">Dashboard</p>
 
@@ -14,9 +14,21 @@ export default function DashboardPage() {
 
             <div className="w-full h-[70svh] flex flex-row items-center">
 
-                <div className="h-3/4 aspect-square bg-foreground/10 rounded-lg outline-1 outline-foreground/25 flex flex-col p-8">
+                <div className="h-3/4 aspect-square bg-foreground/10 rounded-lg outline-1 outline-foreground/25 flex flex-col p-8 gap-2 justify-center">
 
-                    <p className="text-2xl">Hey,<br /><span className="italic font-bold">{currentUser?.user}</span></p>
+                    <p className="mb-auto text-md italic">Welcome Back,<br /><span className="text-2xl font-bold">{currentUser ? currentUser.user : 'Name'}</span></p>
+
+                    <p className="text-2xl font-bold" >0 <span className="text-lg font-normal">Passwords</span></p>
+                    <p className="text-2xl font-bold" >0 <span className="text-lg font-normal">Issues</span></p>
+                    <p className="text-2xl font-bold" >3 <span className="text-lg font-normal">Logins</span></p>
+
+                    <div className="mt-auto w-full h-fit flex flex-row items-center justify-center gap-4">
+
+                        <button className="w-full h-fit p-2 rounded-lg bg-foreground outline-1 outline-foreground/20 hover:bg-foreground/10 hover:text-green-400 duration-200 text-accent">New Password</button>
+                        <button className="w-full h-fit p-2 rounded-lg bg-foreground outline-1 outline-foreground/20 hover:bg-foreground/10 hover:text-blue-400 duration-200 text-accent">Edit Passwords</button>
+
+
+                    </div>
 
                 </div>
 
